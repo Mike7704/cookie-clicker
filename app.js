5f1372685dfbbf7731bff9d0e6d73f8d9a8c8a61
Mike7704 <Michael.cowley2001@gmail.com>
2024-01-18 18:08:45 +0000

    Created layout and added css ready for JavaScript.

---
+++ app.js
@@ -0,0 +1,7 @@
+/*
+let cookies = 0; // Start cookies count at zero
+setInterval(function () {
+  cookies = cookies + 1;
+  console.log("🍪", cookies);
+}, 1000); // 1000 milliseconds == 1 second
+*/
