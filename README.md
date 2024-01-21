2710014ad9f0f4759a71a03c93879b79c2055cb5
Mike7704 <Michael.cowley2001@gmail.com>
2024-01-21 15:22:18 +0000

    Added cookie click power upgrade.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # cookie-clicker
-cookie-clicker
+
+A recreation of the Cookie Clicker game using HTML, CSS and JavaScript.
+
+### Features:
+
+- CSS styling and images.
+- An interactive cookie that produces cookies when clicked.
+- A store to buy items and produce more cookies.
+- Counters that update every second.
+- Local storage to save progress.
--- app.js
+++ app.js
@@ -19,8 +19,9 @@ let items = [
   { owned: 0, name: "Alchemy Lab", production: 1000, cost: 10000 },
   { owned: 0, name: "Bank", production: 2000, cost: 20000 },
   { owned: 0, name: "Time Machine", production: 5000, cost: 50000 },
+  { owned: 1, name: "Double Click Power", production: 1, cost: 50 },
 ];
-// Store start/base costs of each item to reset to
+// Start/base costs of each item to reset to
 let itemsStartCost = [];
 for (let item of items) {
   itemsStartCost.push(item.cost);
@@ -34,12 +35,13 @@ const resetButton = document.getElementById("reset");
 const storeItems = document.querySelectorAll(".store .item");
 const totalCookiesProducedCounter = document.getElementById("total-cookies-produced");
 const totalClicksCounter = document.getElementById("total-clicks");
+const clickAudio = document.getElementById("click-audio");
 
 // Button listeners
 cookieButton.addEventListener("click", cookieClicked);
 resetButton.addEventListener("click", reset);
-// Assign each buy button to its item (Ignoring click power item)
-for (let i = 0; i < items.length; i++) {
+// Assign each buy button to its item
+for (let i = 0; i < storeItems.length; i++) {
   storeItems[i].querySelector("button").addEventListener("click", function () {
     buyItem(i);
   });
@@ -57,6 +59,8 @@ function updateDisplay() {
     storeItems[index].querySelector("#owned").textContent = `${items[index].owned.toLocaleString()}`;
     storeItems[index].querySelector("#cost").textContent = `${items[index].cost.toLocaleString()}`;
   }
+  // Update click power value
+  storeItems[items.length - 1].querySelector("#click-power").textContent = `Click Power: ${clickPower.toLocaleString()}`;
 }
 
 // Increase cookies when cookie button is clicked
@@ -64,6 +68,7 @@ function cookieClicked() {
   cookies += clickPower; // Increase cookies by click power
   clicksPerSecond++; // Track number of clicks for CPS
   totalClicks++; // Track number of total cookie clicks
+  clickAudio.play(); // Play click audio on click
   updateDisplay(); // We want to see the change as we click
 }
 
@@ -78,9 +83,9 @@ function updateCookieCPS() {
   // Reset CPS to recalculate
   cps = 0;
   itemsCPS = 0;
-  // Calculate cps produced by items
-  for (let item of items) {
-    itemsCPS += item.owned * item.production;
+  // Calculate cps produced by items (not including click power)
+  for (let i = 0; i < items.length - 1; i++) {
+    itemsCPS += items[i].owned * items[i].production;
   }
   cps += itemsCPS; // Add CPS from items
   cps += clicksPerSecond * clickPower; // Add CPS from clicks
@@ -94,19 +99,31 @@ function buyItem(index) {
     // Buy the item and remove cookies
     cookies -= items[index].cost;
     items[index].owned++; // Increase item owned amount
-    items[index].cost = Math.floor(items[index].cost * 1.25); // Increase cost by 25% (ignore decimal place)
-    updateDisplay();
+    items[index].cost = Math.floor(items[index].cost * 1.2); // Increase cost by 20% (ignore decimal place)
 
+    // Is click power item?
+    if (items[index].name === "Double Click Power") {
+      // Double click power
+      items[index].production *= 2;
+      clickPower = items[index].production;
+      // Increase cost further
+      items[index].cost = Math.floor(items[index].cost + items[index].owned * clickPower * 3);
+    }
+
+    updateDisplay();
     // Set button to green
-    storeItems[index].querySelector("button").style.backgroundColor = "Green";
-    storeItems[index].querySelector("button").style.color = "white";
-    storeItems[index].querySelector("button").style.textShadow = "none";
+    setItemButton(index, "green");
   } else {
     // Not enough cookies, set button to red
-    storeItems[index].querySelector("button").style.backgroundColor = "Red";
-    storeItems[index].querySelector("button").style.color = "white";
-    storeItems[index].querySelector("button").style.textShadow = "none";
+    setItemButton(index, "red");
   }
+}
+
+// Style item button when clicked
+function setItemButton(index, colour) {
+  storeItems[index].querySelector("button").style.backgroundColor = colour;
+  storeItems[index].querySelector("button").style.color = "white";
+  storeItems[index].querySelector("button").style.textShadow = "none";
 
   // Reset button styling after 1 second
   setTimeout(function () {
@@ -119,13 +136,17 @@ function buyItem(index) {
 // Reset game
 function reset() {
   cookies = 0;
-  clickPower = 1;
   totalCookiesProduced = 0;
   totalClicks = 0;
   for (let i = 0; i < items.length; i++) {
     items[i].owned = 0;
     items[i].cost = itemsStartCost[i];
   }
+  // Click power
+  clickPower = 1;
+  items[items.length - 1].production = 1;
+  items[items.length - 1].owned = 1;
+
   updateLocalStorage();
   updateDisplay();
 }
